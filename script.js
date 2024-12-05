a79752b78ba184fe68e47b7a1f5304db2ffda341
LYASC <ariellayolla@gmail.com>
2024-12-05 16:26:40 +0100

    hauteur et bouton

--- script.js
+++ script.js
@@ -47,4 +47,35 @@ function showResults() {
       break;
     case "C":
       resultText = `Majority of C : Observation<br><br>
-        You pay attention to details and notice small things. <br> <br> You
+        You pay attention to details and notice small things. <br> <br> Your ability to understand situations helps teams work better.`;
+      break;
+    case "D":
+      resultText = `Majority of D : Decision-Making<br><br>
+        You can quickly look at options and make good choices. <br> <br> Your confidence in action is important in difficult situations.`;
+      break;
+    case "E":
+      resultText =
+        "Majority of E : Creativity <br> <br> You have new ideas and think outside the box. <br> <br> Your creativity inspires others and brings fresh solutions.";
+  }
+
+  // Afficher les résultats
+  resultsContainer.innerHTML = resultText;
+  nextButton.style.display = "none";
+}
+
+// Fonction pour gérer les choix
+nextButton.addEventListener("click", () => {
+  // Vérifier que la question actuelle a bien une réponse sélectionnée
+  let selectedAnswer = document.querySelector(
+    `input[name="q${currentQuestionIndex + 1}"]:checked`
+  );
+  if (selectedAnswer) {
+    answers[selectedAnswer.value]++;
+    showNextQuestion();
+  } else {
+    alert("Veuillez répondre à cette question avant de passer à la suivante.");
+  }
+});
+
+// Afficher la première question
+showNextQuestion();
